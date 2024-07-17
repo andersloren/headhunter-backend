c46ebe2ea8beae3d62c24f665c3bfd1cfc4328a3
Liebniz <anders.leif.loren@liebniz.com>
2024-07-17 11:39:16 +0200

    chores: all soft warning removed, except in AccountInfo.java

--- src/main/java/se/sprinta/headhunterbackend/accountInfo/AccountInfo.java
+++ src/main/java/se/sprinta/headhunterbackend/accountInfo/AccountInfo.java
@@ -2,10 +2,13 @@ package se.sprinta.headhunterbackend.accountInfo;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import se.sprinta.headhunterbackend.account.Account;
 
 import java.io.Serializable;
+
+@Getter
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -32,32 +35,16 @@ public class AccountInfo implements Serializable {
         this.account = account;
     }
 
-    public String getId() {
-        return id;
-    }
-
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getOrganization() {
-        return organization;
-    }
-
     public void setOrganization(String organization) {
         this.organization = organization;
-    }
-
-    public Account getAccount() {
-        return account;
     }
 
     public void setAccount(Account account) {
