617a77a6a88297dbdd5e8f0a4a68a91c520796ea
Liebniz <anders.leif.loren@liebniz.com>
2024-07-17 18:59:22 +0200

    chores: lambda function suggestion in applicationRunner

--- src/main/java/se/sprinta/headhunterbackend/HeadhunterBackendApplication.java
+++ src/main/java/se/sprinta/headhunterbackend/HeadhunterBackendApplication.java
@@ -1,26 +1,28 @@
 package se.sprinta.headhunterbackend;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @Log4j2
 @SpringBootApplication
 public class HeadhunterBackendApplication {
 
-    /**
-     * Prints out a message from application*.yml if it is being used for communication with the database.
-     * The purpose for this is debugging.
-     */
     public static void main(String[] args) {
         SpringApplication.run(HeadhunterBackendApplication.class, args);
     }
 
+    /**
+     * Prints out a message from application*.yml if it is being used for communication with the database.
+     * The purpose for this is debugging.
+     */
+
     @Bean
     ApplicationRunner applicationRunner(Environment environment) {
-        return args -> {
-            log.info("Active Configuration File: " + environment.getProperty("active-application-yml"));
-        };
+        return args -> log.info("Active Configuration File: " + environment.getProperty("active-application-yml"));
     }
 }
 
