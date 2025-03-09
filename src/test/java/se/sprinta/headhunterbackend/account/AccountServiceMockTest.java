36a68e413a13d718e420db5e3c623b48893dac67
andersloren <anders.leif.loren@liebniz.com>
2025-03-09 21:36:47 +0100

    chores: included dtoForm array from MockDatabAseInitializer

--- src/test/java/se/sprinta/headhunterbackend/account/AccountServiceMockTest.java
+++ src/test/java/se/sprinta/headhunterbackend/account/AccountServiceMockTest.java
@@ -14,7 +14,7 @@ import se.sprinta.headhunterbackend.MockDatabaseInitializer;
 import se.sprinta.headhunterbackend.account.dto.AccountDtoFormRegister;
 import se.sprinta.headhunterbackend.account.dto.AccountDtoView;
 import se.sprinta.headhunterbackend.account.dto.AccountUpdateDtoForm;
-import se.sprinta.headhunterbackend.system.exception.AccountAlreadyExistException;
+import se.sprinta.headhunterbackend.system.exception.EmailAlreadyExistsException;
 import se.sprinta.headhunterbackend.system.exception.EmailNotFreeException;
 import se.sprinta.headhunterbackend.system.exception.ObjectNotFoundException;
 
@@ -42,11 +42,13 @@ class AccountServiceMockTest {
 
     List<Account> accounts = new ArrayList<>();
     List<AccountDtoView> accountDtos = new ArrayList<>();
+    List<AccountDtoFormRegister> accountDtoFormRegisters = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
         this.accounts = MockDatabaseInitializer.initializeMockAccounts();
         this.accountDtos = MockDatabaseInitializer.initializeMockAccountDtos();
+        this.accountDtoFormRegisters = MockDatabaseInitializer.initializeAccountDtoFormRegister();
     }
 
     @Test
@@ -59,6 +61,10 @@ class AccountServiceMockTest {
         System.out.println("AccountServiceMockTest, accountDtos size: " + this.accountDtos.size());
         for (AccountDtoView accountDto : this.accountDtos) {
             System.out.println(accountDto.toString());
+        }
+        System.out.println("AccountServiceMockTest, accountDtoFormRegisters size: " + this.accountDtoFormRegisters.size());
+        for (AccountDtoFormRegister accountDtoFormRegisters : this.accountDtoFormRegisters) {
+            System.out.println(accountDtoFormRegisters.toString());
         }
     }
 
@@ -253,19 +259,16 @@ class AccountServiceMockTest {
     @Test
     @DisplayName("POST - register - Email Already Exists - Exception")
     void test_RegisterAccount_EmailAlreadyExists_Exception() {
-        // Setup
-        AccountDtoFormRegister newAccountDtoFormRegister = new AccountDtoFormRegister("user1-mock@hh.se", "a", "user");
-
         // Given
-        given(this.accountRepository.findAccountByEmail(newAccountDtoFormRegister.email())).willReturn(Optional.of(this.accounts.get(0)));
+        given(this.accountRepository.findAccountByEmail(this.accountDtoFormRegisters.get(0).email())).willReturn(Optional.of(this.accounts.get(0)));
 
         // When
-        Throwable thrown = assertThrows(AccountAlreadyExistException.class,
-                () -> this.accountService.register(newAccountDtoFormRegister));
+        Throwable thrown = assertThrows(EmailAlreadyExistsException.class,
+                () -> this.accountService.register(this.accountDtoFormRegisters.get(0)));
 
         // Then
         assertThat(thrown)
-                .isInstanceOf(AccountAlreadyExistException.class)
+                .isInstanceOf(EmailAlreadyExistsException.class)
                 .hasMessage("Email is already registered");
     }
 
