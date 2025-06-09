dca16e7a584ade999e555db12fdb9acac321a998
andersloren <anders.leif.loren@liebniz.com>
2025-06-09 19:48:10 +0200

    intialized MessagesMockTest

--- src/test/java/se/sprinta/headhunterbackend/email/MessagesMockTest.java
+++ src/test/java/se/sprinta/headhunterbackend/email/MessagesMockTest.java
@@ -8,19 +8,24 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 import se.sprinta.headhunterbackend.MockDatabaseInitializer;
 import se.sprinta.headhunterbackend.account.Account;
+import se.sprinta.headhunterbackend.verification.Verification;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ActiveProfiles("mock-test")
 @ExtendWith(MockitoExtension.class)
 class MessagesMockTest {
 
     List<Account> accounts = new ArrayList<>();
+    List<Verification> verifications = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
         this.accounts = MockDatabaseInitializer.initializeMockAccounts();
+        this.verifications = MockDatabaseInitializer.initializeMockVerifications();
     }
 
     @Test
@@ -30,6 +35,16 @@ class MessagesMockTest {
         for (Account account : this.accounts) {
             System.out.println(account.toString());
         }
+    }
+
+    @Test
+    @DisplayName("VerificationEmail - Success")
+    void test_VerificationEmail_Success() {
+
+        Map<String, Object> emailPayLoad = new HashMap<>();
+        Map<String, Object> message = new HashMap<>();
+
+
     }
 
 }
